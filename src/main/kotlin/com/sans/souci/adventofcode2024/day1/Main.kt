ff4b8a26db2b6e7793f0d69db5bcbd24a59d9737
msanssouci <mitch.sans.souci@gmail.com>
2024-12-01 23:04:40 -0600

    Complete day 1 part 2.

--- src/main/kotlin/com/sans/souci/adventofcode2024/day1/Main.kt
+++ src/main/kotlin/com/sans/souci/adventofcode2024/day1/Main.kt
@@ -3,26 +3,69 @@ package com.sans.souci.adventofcode2024.day1
 import java.util.PriorityQueue
 import kotlin.math.abs
 
-val minHeap1 = PriorityQueue<Int>()
-val minHeap2 = PriorityQueue<Int>()
 
 /**
- * https://adventofcode.com/2024/day/1
+ * Approximate max storage size is 3N where N is the number of lines in the input file
  */
-fun main() {
-    // Iterate over lines in file puzzle-input.txt
-    object {}.javaClass.getResource("/puzzle-input.txt")!!.readText().lines().forEach {
+data class PuzzleStorage(
+    val minHeapLeft: PriorityQueue<Int>,
+    val minHeapRight: PriorityQueue<Int>,
+    val elementToCountMapLeft: Map<Int, Int>,
+)
+
+/**
+ * Runtime complexity is O(N*(logN + logN)) where N is the number of lines in the input file.
+ */
+fun createPuzzleStorage(inputLines: List<String>): PuzzleStorage {
+    val minHeapLeft = PriorityQueue<Int>()
+    val minHeapRight = PriorityQueue<Int>()
+    val elementToCountMapLeft = mutableMapOf<Int, Int>()
+
+    inputLines.forEach {
         val locationIds = it.split("   ")
-        minHeap1.add(locationIds[0].toInt())
-        minHeap2.add(locationIds[1].toInt())
+        val leftInt = locationIds[0].toInt()
+        val rightInt = locationIds[1].toInt()
+        minHeapLeft.add(leftInt)
+        minHeapRight.add(rightInt)
+
+        elementToCountMapLeft[rightInt] = elementToCountMapLeft.getOrDefault(rightInt, 0) + 1
     }
 
+    return PuzzleStorage(minHeapLeft, minHeapRight, elementToCountMapLeft.toMap())
+}
+
+/**
+ * Runtime complexity is O(N) where N is the number of lines in the input file.
+ */
+fun calculateTotalDistance(puzzleStorage: PuzzleStorage): Int {
     var totalDistance = 0
-    for (i in 0 until minHeap1.size) {
-        val min1 = minHeap1.poll()
-        val min2 = minHeap2.poll()
+    for (i in 0 until puzzleStorage.minHeapLeft.size) {
+        val min1 = puzzleStorage.minHeapLeft.poll()
+        val min2 = puzzleStorage.minHeapRight.poll()
         totalDistance += abs(min2 - min1)
     }
 
-    println("Total Distance: $totalDistance")
+    return totalDistance
+}
+
+/**
+ * Runtime complexity is O(N) where N is the number of lines in the input file.
+ */
+fun calculateSimilarityScore(puzzleStorage: PuzzleStorage): Int {
+    var similarityScore = 0
+
+    for (i in 0 until puzzleStorage.minHeapLeft.size) {
+        val left = puzzleStorage.minHeapLeft.poll()
+
+        similarityScore += puzzleStorage.elementToCountMapLeft.getOrDefault(left, 0) * left
+    }
+
+    return similarityScore
+}
+
+fun main() {
+    // Iterate over lines in file puzzle-input.txt
+    val inputLines = object {}.javaClass.getResource("/puzzle-input.txt")!!.readText().lines()
+    println("Total Distance: ${calculateTotalDistance(createPuzzleStorage(inputLines))}")
+    println("Similarity Score: ${calculateSimilarityScore(createPuzzleStorage(inputLines))}")
 }
